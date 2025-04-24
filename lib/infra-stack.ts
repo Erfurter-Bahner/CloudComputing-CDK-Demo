467efe8ee68c8153ac35e956e1f97407d35c9d24
Vinzenz Geisler <vinzenz.geisler@deutschebahn.com>
2025-04-24 10:49:04 +0200

    Initial commit with CDK and GitHub Actions

---
+++ lib/infra-stack.ts
@@ -0,0 +1,28 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+
+export class InfraStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const helloFunction = new lambda.Function(this, 'HelloFunction', {
+      runtime: lambda.Runtime.NODEJS_18_X,
+      handler: 'index.handler',
+      code: lambda.Code.fromInline(`
+        exports.handler = async function(event) {
+          console.log("Request received");
+          return {
+            statusCode: 200,
+            body: JSON.stringify({ message: 'Hallo von der GitHub Actions CDK Demo! 🚀' })
+          };
+        };
+      `),
+    });
+
+    new apigateway.LambdaRestApi(this, 'Endpoint', {
+      handler: helloFunction
+    });
+  }
+}
