1c0f0bb10777dcb70550f597b153f6bad8de7361
Vinzenz Geisler <vinzenz.geisler@deutschebahn.com>
2025-05-14 09:05:30 +0200

    new text

--- lib/infra-stack.ts
+++ lib/infra-stack.ts
@@ -15,7 +15,7 @@ export class InfraStack extends cdk.Stack {
           console.log("Request received");
           return {
             statusCode: 200,
-            body: JSON.stringify({ message: 'Hallo Kasche' })
+            body: JSON.stringify({ message: 'Hallo PI23' })
           };
         };
       `),
